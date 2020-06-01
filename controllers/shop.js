e10231141c556ba14d4ae385219e730ee75de747
iliyan <iliyan.tsachev@ilnweb.com>
2020-06-01 12:30:29 +0200

    creating the cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
 	Product.fetchAll((products) => {
@@ -20,11 +21,30 @@ exports.getProducts = (req, res, next) => {
 	});
 };
 
+exports.getProduct = (req, res, next) => {
+	const prodId = req.params.productId;
+	Product.findById(prodId, (product) => {
+		res.render('shop/product-detail', {
+			product: product,
+			pageTitle: product.title,
+			path: '/products'
+		});
+	});
+};
+
 exports.getCart = (req, res, next) => {
 	res.render('shop/cart', {
 		pageTitle: 'Your Cart',
 		path: '/cart'
 	});
+};
+
+exports.postCart = (req, res, next) => {
+	const prodId = req.body.productId;
+	Product.findById(prodId, (product) => {
+		Cart.addProduct(prodId, product.price);
+	});
+	res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
