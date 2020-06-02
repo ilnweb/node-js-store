06a6ed9de3fdc2faabd1ad7caf3d431c4462bd01
iliyan <iliyan.tsachev@ilnweb.com>
2020-06-02 16:18:52 +0200

    mysql queries for products

--- util/database.js
+++ util/database.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: 'codemode8894'
+    host: 'localhost',
+    user: 'root',
+    database: 'node-complete',
+    password: 'codemode8894'
 });
 
 module.exports = pool.promise();
