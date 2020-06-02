935897a08f058a8819926bbf243fe433216c5f75
iliyan <iliyan.tsachev@ilnweb.com>
2020-06-02 17:45:02 +0200

    convering the app to use sequalize

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const path = require('path');
-
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const errorController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 const app = express();
+
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -21,10 +22,15 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then((result) => {
-  // console.log(result);
-  app.listen(4000);
-}).catch((err) => {
-	console.log(err);
-});
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
 
+sequelize
+  .sync({force:true})
+  .then(result => {
+    // console.log(result);
+    app.listen(4000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
--- util/database.js
+++ util/database.js
@@ -2,5 +2,4 @@ const Sequelize = require('sequelize');
 
 const sequelize = new Sequelize('node-complete', 'root', 'codemode8894', { dialect: 'mysql', host: 'localhost' });
 
-
 module.exports = sequelize;
