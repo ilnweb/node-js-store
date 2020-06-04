a490531a4cd22d80a67549a7ca25b5ed8e28bd28
iliyan <iliyan.tsachev@ilnweb.com>
2020-06-04 18:50:57 +0200

    convertin everything to work with session and cookie

--- app.js
+++ app.js
@@ -5,8 +5,13 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 const mongoose = require('mongoose');
 const session = require('express-session');
+const MongoDBStore = require('connect-mongodb-session')(session);
+const MNGODB_URI = 'mongodb+srv://iliyan:codemode8894@cluster0-s4kfe.mongodb.net/shop?retryWrites=true&w=majority';
 const app = express();
-
+const store = new MongoDBStore({
+	uri: MNGODB_URI,
+	collection: 'sessions'
+});
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
@@ -16,16 +21,19 @@ const authRoutes = require('./routes/auth');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({ secret: 'secret key', resave: false, saveUninitialized: false }));
+app.use(session({ secret: 'secret key', resave: false, saveUninitialized: false, store: store }));
 
 app.use((req, res, next) => {
-	User.findById('5ed8d0d4403687647c5e38e5')
-		.then((user) => {
-			req.user = user;
-			next();
-		})
-		.catch((err) => console.log(err));
-});
+  if (!req.session.user) {
+    return next();
+  }
+  User.findById(req.session.user._id)
+  .then((user) => {
+    req.user = user;
+    next();
+  })
+  .catch((err) => console.log(err));
+})
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -34,7 +42,7 @@ app.use(authRoutes);
 app.use(errorController.get404);
 
 mongoose
-	.connect('mongodb+srv://iliyan:codemode8894@cluster0-s4kfe.mongodb.net/shop?retryWrites=true&w=majority')
+	.connect(MNGODB_URI)
 	.then((result) => {
 		User.findOne().then((user) => {
 			if (!user) {
